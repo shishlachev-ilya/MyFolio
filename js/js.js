e7fd4ce371bb7ea918cb5ee15bebcbf251a0370f
Shishlachev-ilya <mailforwot43@mail.ru>
2016-07-30 22:27:49 +0300

    finished

---
+++ js/js.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        $('.animated').each(function() {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 700) {
+                $('.down').addClass('bounceInDown');
+                $('.left').addClass('bounceInLeft');
+                $('.right').addClass('bounceInRight');
+                $('.up').addClass('bounceInUp');
+            }
+        });
+    });
+
+    function menu() {
+
+        var trig = $('.trigger-js'),
+            menu = $('.menu-js');
+
+        trig.click(function() {
+            $(this).next(menu).slideToggle();
+        });
+
+        $(window).resize(function() {
+
+            if ($(window).width() > 732) {
+                menu.removeAttr('style');
+            }
+        });
+    }
+    menu();
+
+
+});
